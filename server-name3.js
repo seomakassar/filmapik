260e1ddfaa3c8d8c5c6cef632d46d19c71b47cce
seomakassar <47053438+seomakassar@users.noreply.github.com>
2026-07-02 01:27:54 +0800

    Add files via upload

---
+++ server-name3.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+function getServerName(url){
+    try{
+        let u = new URL(url);
+
+        // Jika menggunakan endpoint player.php, ambil URL asli
+        if(u.pathname.endsWith("/player.php")){
+            const realUrl = u.searchParams.get("url");
+            if(realUrl){
+                u = new URL(decodeURIComponent(realUrl));
+            }
+        }
+
+        const host = u.hostname.replace(/^www\./,"").toLowerCase();
+
+        if(host.includes("byseqekaho")) return "FILEMOON";
+        if(host.includes("strp2p")) return "STREAMP2P";
+        if(host.includes("abyssplayer")) return "HYDRAX";
+        if(host.includes("efek.stream")) return "VIP SERVER";
+        if(host.includes("buzzheavier")) return "BUZZHEAVIER";
+        if(host.includes("send.now")) return "SEND.CM";
+        if(host.includes("q8y5z")) return "Q8Y5Z";
+
+        return host.split(".")[0].toUpperCase();
+
+    }catch(e){
+        return "DOWNLOAD";
+    }
+}
+
+	// Link player
+	document.querySelectorAll("#player-list .player-option").forEach(link => {
+
+		const name = getServerName(link.dataset.url || link.href);
+
+		link.textContent = name;
+		link.dataset.server = name;
+
+	});
+
+	// Dropdown player
+	document.querySelectorAll("#player-select option").forEach(option => {
+
+		const name = getServerName(option.value);
+
+		option.textContent = `SERVER ${name}`;
+		option.dataset.server = name;
+
+	});
+
+});
